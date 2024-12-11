6ec70d2ddd82ec610d3f76d043b70f3affa45584
moamer-dev <mohamed.awad.amer@gmail.com>
2024-12-12 00:27:27 +0100

    chore: create factories

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'slug' => fake()->slug(),
+            'is_active' => 1,
+            'parent_id' => null,
+        ];
+    }
+}
---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Course;
+use App\Models\User;
+use App\Models\Level;
+use App\Models\Category;
+use App\Models\Currency;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class ProjectFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $user = User::first();
+        $level = Level::first();
+        $category = Category::first();
+
+        return [
+            'name' => fake()->name(),
+            'slug' => fake()->slug(),
+            'description' => fake()->text(),
+            'short_title' => fake()->sentence(),
+            'creator_id' => $user->id,
+            'level_id' => $level->id,
+            'tags' => fake()->words(5, true),
+            'status' => 'published',
+            'duration' => random_int(1, 12),
+            'price_type' => 'free',
+        ];
+    }
+}
